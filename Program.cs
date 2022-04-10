de27f5ac77c3964d4dd174d0c1ef25e8a4d837e2
Hugo Kjellerstedt <hugo.kjellerstedt@gmail.com>
2022-04-10 09:13:47 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace WebbedAudio_AIP__Application_In_Parts_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://www.youtube.com/"; //make possible to input multiple links
+            string downloadFolder = @"C:\Users\hugok\source\repos\WebbedAudio AIP (Application In Parts)\Downloads\";
+            List<Tuple<string, int>> subsites = Scraper.GetSubsites(url);
+           
+            subsites.Insert(0, new Tuple<string, int>(url, subsites.Count)); //inserts first url because GetSubsites() only gets subsites, not og site
+            subsites = subsites.OrderBy(x => x.Item2).ToList(); //sorts list by Item 2 (site number)
+
+            for (int i = 0; i < subsites.Count; i++)
+            {
+                Tuple<string, int> subsite = subsites[i];
+                url = subsite.Item1;
+
+                List<MediaItem> mediaElements = Scraper.GetMediaElements(url);
+
+                foreach (var media in mediaElements)
+                {
+                    Console.WriteLine($"found mp3 files: {media.Title}");
+                }
+
+                Scraper.DownloadMedia(mediaElements, downloadFolder);
+                Console.WriteLine(subsite.Item2 == subsites.Count - 1 ? //displays different message depending on which sites media is being downloaded
+                    $"Final media elements downloaded." : $"media elements downloaded from {i+1} site, {subsites.Count - i - 1} sites remaining...."); 
+            }
+
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ Scraper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using ScrapySharp.Extensions;
+using HtmlAgilityPack;
+using System.Net;
+using System.IO;
+
+namespace WebbedAudio_AIP__Application_In_Parts_
+{
+    public class Scraper
+    {
+        static string GetSiteHtml(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = client.GetAsync(url).Result) //gets response from requested url
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        return content.ReadAsStringAsync().Result; //gets content (html)
+                    }
+                }
+            }
+        }
+
+        public static List<Tuple<string, int>> GetSubsites(string url)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(GetSiteHtml(url));
+
+            List<Tuple<string, int>> subsites = new List<Tuple<string, int>>();
+            int numbSubsites = 0;
+
+            var mediaElementNodes = doc.DocumentNode.CssSelect("div.page-links > a");
+
+            foreach (var mediaNode in mediaElementNodes)
+            {
+                string link = mediaNode.Attributes["href"].Value;
+                subsites.Add(new Tuple<string, int>(link, numbSubsites));
+                numbSubsites++;
+            }
+
+            return subsites;
+        }
+
+        public static List<MediaItem> GetMediaElements(string url)
+        {
+            List<MediaItem> mediaElements = new List<MediaItem>();
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(GetSiteHtml(url));
+
+            var mediaElemenNodes = doc.DocumentNode.CssSelect("audio.wp-audio-shortcode > a");
+
+            foreach (var node in mediaElemenNodes)
+            {
+                string link = node.InnerHtml.CleanInnerText(); //gets text inside tag (<a> this text </a>)
+                mediaElements.Add(new MediaItem(link));
+            }
+            
+            return mediaElements;
+        }
+
+        public static void DownloadMedia(List<MediaItem> mediaItems, string downloadFolder)
+        {
+            WebClient client = new WebClient();
+
+            foreach (var media in mediaItems)
+            {
+                string downloadFile = $@"{downloadFolder}{media.Title}.mp3";
+                using (StreamWriter sw = new StreamWriter(downloadFile)) //we need to create file before initializing content
+                {
+                }
+                client.DownloadFile(new Uri(media.FullUrl), downloadFile); //initializes content
+            }
+        }
+    }  
+}
